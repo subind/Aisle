5c9a36fe81b426ca11df8d28f08442ddedc6c619
VSUBISU <subind.subind@pearson.com>
2022-08-19 06:44:57 +0530

    Added dependencies for Jetpack navigation component Dimens provided, gitIgnore updated, styles extracted

---
+++ app/src/main/java/com/example/aisle/ui/PhoneNumberFragment.kt
@@ -0,0 +1,21 @@
+package com.example.aisle.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.aisle.R
+
+class PhoneNumberFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_phone_number, container, false)
+    }
+
+}
